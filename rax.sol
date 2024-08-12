f2b6f545122d59de6f3a9cd33ee895515e3c3dfa
Radoslav Aleksiev <136315431+RadoslavAleksiev@users.noreply.github.com>
2024-08-13 01:08:42 +0300

    Update rax.sol

--- rax.sol
+++ rax.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RAX is ERC20 {
+    constructor() ERC20("RAX", "RAX") {
+        // Mint the initial supply of tokens to the deployer
+        _mint(msg.sender, 999000000 * 10 ** decimals());
+    }
+}
